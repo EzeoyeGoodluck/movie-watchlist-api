697454c20daa7afc3de893f2a966eb5b2f87e9d4
Ezeoye Goodluck <116758130+EzeoyeGoodluck@users.noreply.github.com>
2026-08-18 08:28:17 -1200

    setting up , our routes

--- src/server.js
+++ src/server.js
@@ -2,8 +2,29 @@ const express = require("express");
 
 const app = express();
 
+app.get("/hello" , (req,  res) => {
+    res.json({message: "Hello world"})
+
+});
+
 const PORT = 5001;
 
 const server = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+
+
+//GET, POST, PUT, DELETE
+//http://localhost:5001/hello
+// json light weight text format that will aloow you to store data using key value pairs 
+
+//AUTH - SIGNIN, SIGNOUT
+
+// MOVIE -N GETTING ALL MOVIES 
+
+// USER - PROFILE
+
+//WATCHLIST 
+
+//DELETE MOVIES 
